07070e480e9cac2f527a10196d0f573fd7614525
russnguyen72 <227737184+russnguyen72@users.noreply.github.com>
2026-05-08 19:09:49 -0700

    completed implementation of tree.h and created developer_log.md to track progress

---
+++ developer_log.md
@@ -0,0 +1,9 @@
+# AI-Generated Adventure
+## CS 210 (Spring 26)
+
+---
+
+### Entry 1
+#### Date: 5/8/2026
+
+I completed the implementation of the Tree class located in `tree.h` by fleshing out the methods used in the class and created this developer log to track my progress within this project. I have not yet deleted the TODO comments so I can later compare the actual functionality of the class to its intended functionality once I have developed the main.cpp file and begin testing the code.
--- tree.h
+++ tree.h
@@ -6,6 +6,7 @@
 #define FA25EC3_TREE_H
 
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
 using namespace std;
@@ -38,7 +39,7 @@ public:
     vector<Node<U>*> children;
 
     // TODO: Write constructor
-    // Node(const string &nodeID, const U &value);
+    Node(const string &nodeID, const U &value) : id(nodeID), data(value) {}
 };
 
 template <typename T>
@@ -47,23 +48,65 @@ private:
     Node<T>* root;
 
 public:
-    Tree();
+    Tree() : root(nullptr) {}
     // TODO: Initialize root pointer to nullptr
 
-    void createRoot(const string &id, const T &value);
+    void createRoot(const string &id, const T &value) {
+        if (root) return;
+        root = new Node<T>(id, value);
+    }
     // TODO: Allocate memory, assign id, assign data, set as root
 
-    void addNode(const string &parentID, const string &childID, const T &value);
+    bool addNode(const string &parentID, const string &childID, const T &value) {
+        if (!root) return false;
+        if (root->id == parentID) {
+            root->children.push_back(new Node<T>(childID, value));
+            return true;
+        }
+        else {
+            for (Node<T>* child : root->children) {
+                if (child->addNode(parentID, childID, value)) return true;
+            }
+        }
+
+        return false;
+    }
     // TODO: Find parent, create child, link parent to child
     // TODO: Support repeated children under multiple parents
 
-    Node<T>* findNode(const string &id);
+    Node<T>* findNode(const string &id) {
+        if (!root) return nullptr;
+        if (root->id == id) return root;
+        else {
+            for (Node<T>* child : root->children) {
+                Node<T>* temp = child->findNode(id);
+                if (temp) return temp;
+            }
+        }
+
+        return nullptr;
+    }
     // TODO: Use DFS or BFS to search tree
 
-    void printAll();
+    void printAll() {
+        if (!root) return;
+        queue<Node<T>*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            Node<T>* temp = q.front();
+            q.pop();
+            cout << temp->id << ": " << temp->data << endl;
+            for (Node<T>* child : temp->children) {
+                q.push(child);
+            }
+        }
+    }
     // TODO: Print entire structure in readable form
 
-    ~Tree();
+    ~Tree() {
+        delete root;
+    }
     // TODO: Free all allocated memory
 };
 
